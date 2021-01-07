e8d07128282d5c077748a8fe439146e975b278b8
Eduardo <drdpedroso@gmail.com>
2021-01-07 20:50:01 -0300

    Getting more stuff done

--- content.js
+++ content.js
@@ -10,41 +10,63 @@ const setState = (newState) => {
     setPins(state.pins)
 }
 
+const pinWasClicked = (pin) => {
+    const modalElm = document.getElementById(`modal-${pin.id}`)
+    if (!modalElm) {
+
+    } else {
+        if (modalElm.style.display === 'none') {
+            modalElm.style.display = 'block'
+            return
+        }
+        modalElm.style.display = 'none'
+    }
+}
+
 const setPins = (pins) => {
     pins.forEach((pin) => {
         const elm = createElementFromHTML(`
-            <div style="top:${pin.y}px;left: ${pin.x}px;position: absolute; font-size: 23px;z-index: 233;">XOT</div>
+            <div style="top:${pin.y}px;left: ${pin.x}px;position: absolute; font-size: 23px;z-index: 233;">LEK</div>
         `)
         document.body.appendChild(elm)
         document.getElementById('overlay').style.display = 'none'
+        document.getElementById('ex-sidebar').style.display = 'block'
     })
 }
 
-
-
 const init = () => {
     const div = createElementFromHTML(`
-        <div class="wrapper">
-            <div class="sidebar">
+        <div class="ext-wrapper" id="ex-sidebar">
+            <div class="ext-sidebar">
                 <button id="btn"> + </button>
             </div>
         </div>
     `)
 
     const div2 = createElementFromHTML(`
-        <div class="clickable-overlay" id="overlay" style="display: none"></div>
+        <div class="ext-clickable-overlay" id="overlay" style="display: none"></div>
     `)
+    const m = createElementFromHTML(`
+        <div class="ext-modal-overlay" style="display: none">
+            <div class="ext-modal" >
+                <div>
+                ds
+                </div>
+            </div>
+        </div>
+    `)
+    document.body.appendChild(m)
     document.body.appendChild(div)
     document.body.appendChild(div2)
-
-
 }
+
  function createElementFromHTML(htmlString) {
      let div = document.createElement('div')
      div.innerHTML = htmlString.trim()
 
      return div.firstChild;
  }
+
 init()
 //
  document.getElementById('overlay').addEventListener('click', function clickEvent(e) {
@@ -52,10 +74,11 @@ init()
     const y = e.pageY;
 
     setState({
-        pins: new Set([...state.pins, { x, y }])
+        pins: new Set([...state.pins, { x, y, id: 'lek' }])
     })
 }, true)
 
  document.getElementById('btn').addEventListener('click', function clickEvent(e) {
      document.getElementById('overlay').style.display = 'block'
+     document.getElementById('ex-sidebar').style.display = 'none'
  }, true)
